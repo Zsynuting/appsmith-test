0318df6cbd3d2d1139bc45328cae2e75f3c08ebd
Zsynuting <zsynuting>
2023-09-06 07:14:52 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/student/student.js
@@ -0,0 +1,17 @@
+export default {
+	name: 'abc',
+	gotoschool() {
+		console.log('going to school')
+	},
+
+	async myFun2 () {
+		debugger
+		return {
+			x: {
+				y: {
+					z: 1
+				}
+			}
+		}
+	},
+}
